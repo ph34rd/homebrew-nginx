4d8254384dd248984d8c0ce1673611f08dd52b89
Yuriy Glukhov <ph34rd@ruswv.com>
2014-10-29 13:28:50 +0300

    class name fix

--- Formula/arutrtmp-nginx-module.rb
+++ Formula/arutrtmp-nginx-module.rb
@@ -1,6 +1,6 @@
 require "formula"
 
-class NginxRtmpModule < Formula
+class ArutrtmpNginxModule < Formula
   homepage "https://github.com/arut/nginx-rtmp-module"
   url "https://github.com/arut/nginx-rtmp-module/archive/v1.1.5.tar.gz"
   sha1 "7500b4bd90fc942468f94dd3a12dbcdf9c3199a6"
